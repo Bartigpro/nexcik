d5be47071e13e5378bac1969df4b8b1e700bc205
Bartosz Gańko <Bartosz.ganko@wp.pl>
2024-10-10 17:02:22 +0200

    sygma kolor

--- src/app/strona1/[kod]/page.js
+++ src/app/strona1/[kod]/page.js
@@ -6,6 +6,8 @@ import Link from "next/link"; // Import Link for navigation
 export default function Info({ params }) {
     const [kraj, setKraj] = useState();
     const [neighbours, setNeighbours] = useState([]);
+    const [maxp, setMaxp] = useState(null)
+    const [minp, setMinp] = useState(null)
 
     useEffect(() => {
         async function getData() {
@@ -16,13 +18,25 @@ export default function Info({ params }) {
 
                 // Fetch neighboring countries
                 if (jsonres[0].borders) {
-                    const bordersResponse = await fetch(`https://restcountries.com/v3.1/alpha?codes=${jsonres[0].borders.join(',')}`);
+                    const bordersResponse = await fetch(`https://restcountries.com/v3.1/alpha?codes=${jsonres[0].borders}`);
                     const bordersJson = await bordersResponse.json();
+                    console.log(bordersJson)
                     setNeighbours(bordersJson);
+
+                    const pops = bordersJson.map(kraj => kraj.population)
+                    const maxpopulation = Math.max(...pops) 
+                    const minpopulation = Math.min(...pops) 
+                    console.log("max: " && maxpopulation)
+                    console.log("max: " && minpopulation)
+                    setMaxp(maxpopulation)
+                    setMinp(minpopulation)
+
+
                 }
             } catch (error) {
                 console.log(error);
             }
+            
         }
         getData();
     }, [params.kod]);
@@ -44,18 +58,21 @@ export default function Info({ params }) {
 
                     {neighbours.length > 0 && (
                     <div>
-                    <h2>Neighboring Countries:</h2>
+                    <h2>sąsiednie sigmy:</h2>
                     <div className="flex flex-wrap justify-center">
                         {neighbours.map(neighbour => (
-                    <Link key={neighbour.cca2} href={`/strona1/${neighbour.cca2}`} className="m-2 p-2 border border-gray-300 rounded">
+                    <Link key={neighbour.cca2} href={`/strona1/${neighbour.cca2}`}  className={`m-2 p-2 border border-gray-300 rounded ${neighbour.population == maxp ? 'bg-green-500' : neighbour.population == minp ? 'bg-red-500' : ''}`}>
                     <div>
                     <Image
                     src={neighbour.flags.png}
-                    width="50"
-                     height="30"
-               
+                    width="200"
+                     height="120"
+                            
                         />
+                       
                       <p>{neighbour.name.common}</p>
+                      <p>{neighbour.cca2}</p>
+                      <p>{neighbour.population}</p>
                          </div>
                     </Link>
                  ))}
